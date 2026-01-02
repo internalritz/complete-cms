c274c1e5c43fb4ebffe364e77d06edbdddf09f62
internalritz <it@ritzcollege.edu.np>
2026-01-02 10:32:00 +0545

    database for production

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,24 +1,10 @@
 
 
-module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "cms",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-  };
- 
 // module.exports = {
-//     HOST: "mysql://root:yDsHOQEClRfwcBZPEmqMPskasrlIElMO@trolley.proxy.rlwy.net:50516/railway",
+//     HOST: "localhost",
 //     USER: "root",
-//     PASSWORD: "yDsHOQEClRfwcBZPEmqMPskasrlIElMO",
-//     DB: "railway",
+//     PASSWORD: "",
+//     DB: "cms",
 //     dialect: "mysql",
 //     pool: {
 //       max: 5,
@@ -27,3 +13,17 @@ module.exports = {
 //       idle: 10000,
 //     },
 //   };
+ 
+module.exports = {
+    HOST: "mysql://root:yDsHOQEClRfwcBZPEmqMPskasrlIElMO@trolley.proxy.rlwy.net:50516/railway",
+    USER: "root",
+    PASSWORD: "yDsHOQEClRfwcBZPEmqMPskasrlIElMO",
+    DB: "railway",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  };
